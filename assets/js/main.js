caf536dae9650e0181576394d1922b5999d8a3bf
Axay Patel <Patel.2788@buckeyemail.osu.edu>
2021-07-16 14:14:29 -0400

    added swiper

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,20 +48,38 @@ const skillsContent = document.getElementsByClassName('skills__content'),
         el.addEventListener('click', toggleSkills)
       })
 
-      
 
-
-/*==================== QUALIFICATION TABS ====================*/
-
-
-/*==================== SERVICES MODAL ====================*/
-
-
-/*==================== PORTFOLIO SWIPER  ====================*/
-
-
-/*==================== TESTIMONIAL ====================*/
-
+/*==================== SURPRISE SWIPER  ====================*/
+// Swiper Configuration
+var swiper = new Swiper(".swiper-container", {
+    slidesPerView: 1.5,
+    spaceBetween: 10,
+    centeredSlides: true,
+    freeMode: true,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 1
+      },
+      700: {
+        slidesPerView: 1.5
+      }
+    }
+  });
+  
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
